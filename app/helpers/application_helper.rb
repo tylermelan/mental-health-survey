0e11f6d69155c17176fc91d1be2ceea2d13a064f
Tyler Melan <tymelan@gmail.com>
2024-11-03 07:00:29 -0800

    setup react

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def react_component(component)
+    tag.div(id: "react-container", data: { component: })
+  end
 end
---
+++ app/javascript/react/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Survey } from "./pages/Survey";
+
+function getRegisteredComponent(componentName: string) {
+  const registeredComponents = new Map([[Survey.name, Survey]]);
+
+  return registeredComponents.get(componentName);
+}
+
+function setupReact() {
+  const reactContainer = document.getElementById("react-container");
+
+  if (!reactContainer) return;
+
+  const componentName = reactContainer.dataset.component;
+
+  if (!componentName) {
+    console.error(
+      'No component set on react container. Usage: <div id="react-container" component="Component"></div>'
+    );
+    return;
+  }
+
+  const Component = getRegisteredComponent(componentName);
+
+  if (!Component) {
+    console.error(
+      `${componentName} not found. Make sure ${componentName} is defined and registered`
+    );
+    return;
+  }
+
+  const root = createRoot(reactContainer);
+  root.render(<Component />);
+}
+
+setupReact();
